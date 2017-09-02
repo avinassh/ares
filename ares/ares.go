f807def1b52888c53132100021c842aa51bae5ca
avi <hi@avi.im>
2017-09-02 21:32:01 +0530

    Add notify user code

--- ares/ares.go
+++ ares/ares.go
@@ -20,7 +20,43 @@ func (a *Ares) deleteFile(fileId string) {
 	}
 }
 
-func (a *Ares) handleFile(file *slack.File) {
+func (a *Ares) notifyUser(user, deleteHash string) {
+	api := slack.New(a.SlackBotToken)
+	params := slack.PostMessageParameters{}
+	attachment := slack.Attachment{
+		Color:   "danger",
+		Pretext: "Hey there, image uploads to this slack is disabled. Please use external image upload services next time. This incident has been reported :squirrel:",
+		Text:    "I have uploaded the image to Imgur for now.",
+		Fields: []slack.AttachmentField{
+			{
+				Title: "If you wish to delete the image uploaded, click on the following link",
+				Value: fmt.Sprintf("https://imgur.com/delete/%s", deleteHash),
+			},
+		},
+	}
+	params.Attachments = []slack.Attachment{attachment}
+	_, _, err := api.PostMessage(user, "", params)
+	if err != nil {
+		log.Println("Failed to send DM to user", err.Error())
+	}
+}
+
+func (a *Ares) sendImgToSlack(channel, user, url string) {
+	api := slack.New(a.SlackBotToken)
+	params := slack.PostMessageParameters{}
+	attachment := slack.Attachment{
+		Color:    "good",
+		Text:     fmt.Sprintf("image originally uploaded by <@%s>", user),
+		ImageURL: url,
+	}
+	params.Attachments = []slack.Attachment{attachment}
+	_, _, err := api.PostMessage(channel, "", params)
+	if err != nil {
+		log.Println("Failed to send DM to user", err.Error())
+	}
+}
+
+func (a *Ares) handleFile(file *slack.File, channel string) {
 	resp := uploadToImgur(file.URLPrivateDownload, a.SlackAppToken, a.ImgurClientID)
 
 	if resp.Status != 200 {
@@ -28,6 +64,8 @@ func (a *Ares) handleFile(file *slack.File) {
 		return
 	}
 
+	a.notifyUser(file.User, resp.Data.Deletehash)
+	a.sendImgToSlack(channel, file.User, resp.Data.Link)
 	a.deleteFile(file.ID)
 }
 
@@ -52,7 +90,7 @@ func (a *Ares) Run() {
 		case *slack.MessageEvent:
 			if ev.SubType == "file_share" {
 				if isImageFile(ev.File.Filetype) {
-					a.handleFile(ev.File)
+					a.handleFile(ev.File, ev.Channel)
 				}
 			}
 
