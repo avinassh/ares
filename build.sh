2ae52c0a1945c313d091893fbbb5ce0be3874164
avi <hi@avi.im>
2018-02-09 19:49:51 +0530

    Update build file

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
 CGO_ENABLED=0 GOOS=linux go build -ldflags "-s" -a -installsuffix cgo -o build/ares .
-docker build . -f Dockerfile -t ares
+docker build . -f Dockerfile -t ares -t avinassh/ares:latest
+# docker push avinassh/ares:latest
